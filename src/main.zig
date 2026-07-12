02bf8a4293736777f1b63e5d721bffdb7303b659
unknown <kindermolina98@gmail.com>
2026-07-11 23:35:33 -0300

    starting the cli;

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,30 @@
 const std       = @import("std");
-//const display   = @import("controllers/utils.zig");
-//const init      = @import("init.zig");
-//const print     = @import("std").debug.print;
-//const mem       = @import("std").mem;
+
+const commands = [_][]const u8{
+    "l",
+    "s",
+    "d",
+};
+
+const Command = struct {
+    name: []const u8,
+};
+
+
+pub fn worker(cmd:Command) !void {
+    if (std.mem.eql(u8, cmd.name, "l")) {
+        const cwd = std.fs.cwd();
+        var dir = try cwd.openDir(".", .{ .iterate = true });
+        defer dir.close();
+        var it = dir.iterate();
+
+        while (try it.next()) |entry| {
+            std.debug.print("{s} ({any})\n", .{ entry.name, entry.kind });
+        }
+    } else {
+        std.debug.print("\nworkgin\n", .{});
+    }
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -12,8 +34,24 @@ pub fn main() !void {
 
     var args = try std.process.argsWithAllocator(allocator);
     defer args.deinit();
+    var found = false;
 
+    var cmd: Command = undefined;
+    _ = args.next();
     while (args.next()) |arg| {
-        std.debug.print("{s}\n", .{arg});
+        for (commands) |command| {
+            if (std.mem.eql(u8, command, arg)) {
+                found = true;
+                std.debug.print("Command is available\n", .{});
+                cmd.name = arg;
+                break;
+            }
+        }
+    } 
+    if (found == false) {
+        std.debug.print("Command is not available\n", .{});
+        return;
     }
+    try worker(cmd);
+
 }
