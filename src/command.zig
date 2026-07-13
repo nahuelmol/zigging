82e196ae361ecfdfdf94ea714f1f6173b4d82feb
unknown <kindermolina98@gmail.com>
2026-07-13 14:18:30 -0300

    root, target and typetarget working;

--- src/command.zig
+++ src/command.zig
@@ -10,6 +10,7 @@ pub const Command = struct {
     allocator: std.mem.Allocator,
     root: []const u8,
     target: []const u8,
+    typetarget: []const u8,
 
     pub fn init(self: *Command, allocator: std.mem.Allocator) void {
         self.allocator = allocator;
@@ -20,8 +21,12 @@ pub const Command = struct {
     pub fn deinit(self: *Command) void {
         self.allocator.free(self.root);
 
-        if (self.target.len != 0 and !std.mem.eql(u8, self.target, ".")) {
+        if (self.target.len != 1 or self.target[0] != '.') {
             self.allocator.free(self.target);
+        }
+
+        if (!std.mem.eql(u8, self.typetarget, "all")) {
+            self.allocator.free(self.typetarget);
         }
     }
 
@@ -32,7 +37,6 @@ pub const Command = struct {
         defer std.process.argsFree(self.allocator, argv);
 
         var found = false;
-        var i:usize =   1;
 
         _ = args.next();
         const nargs = argv.len - 1;
@@ -41,21 +45,26 @@ pub const Command = struct {
         for (commands) |command| {
             if (std.mem.eql(u8, command, root)) {
                 found = true;
-                std.debug.print("Root found\n", .{});
                 self.root = try self.allocator.dupe(u8, root);
                 break;
             }
         }
-        i+=1;
         if (found == true) {
             if (std.mem.eql(u8, "l", self.root)) {
-                if (nargs > (i+1)){
-                    self.target = argv[i+1];
+                if (nargs > 1){
+                    self.target = try self.allocator.dupe(u8, argv[2]);
+                    if (nargs > 2){
+                        self.typetarget = try self.allocator.dupe(u8, argv[3]);
+                    } else {
+                        self.typetarget = "all";
+                    }
                 } else {
                     self.target = ".";
                 }
             } else if (std.mem.eql(u8, "d", self.root)) {
                 std.debug.print("setting for d command", .{});
+            } else if (std.mem.eql(u8, "s", self.root)) {
+                std.debug.print("setting for s command", .{});
             } else {
                 std.debug.print("unrecognized root", .{});
             }
@@ -63,6 +72,6 @@ pub const Command = struct {
             std.debug.print("Command is not available\n", .{});
             return;
         }
-        std.debug.print("setting up command\n root {s} ", .{self.root});
+        std.debug.print("command setted\n", .{});
     }
 };
--- src/controllers/worker.zig
+++ src/controllers/worker.zig
@@ -3,17 +3,24 @@ const Command = @import("..\\command.zig").Command;
 
 pub fn Worker(cmd:Command) !void {
     var targetpath:[]const u8 = "";
+    std.debug.print("\nroot:{s}\ntarget:{s}\ntypetarget:{s}\n", .{ cmd.root, cmd.target, cmd.typetarget });
     if (std.mem.eql(u8, cmd.root, "l")) {
         if(std.mem.eql(u8, cmd.target, "download") or std.mem.eql(u8, cmd.target, "d")){
             targetpath = "C:\\Users\\USUARIO\\Downloads";
         }
-        const cwd = std.fs.cwd();
-        var dir = try cwd.openDir(targetpath, .{ .iterate = true });
+        const cwd   = std.fs.cwd();
+        var dir     = try cwd.openDir(targetpath, .{ .iterate = true });
         defer dir.close();
         var it = dir.iterate();
 
         while (try it.next()) |entry| {
-            std.debug.print("{s} ({any})\n", .{ entry.name, entry.kind });
+            if(std.mem.eql(u8, cmd.typetarget, "all")){
+                std.debug.print("{s} ({any})\n", .{ entry.name, entry.kind });
+            } else if(std.mem.eql(u8, cmd.typetarget, "P")){
+                if(std.mem.eql(u8, entry.name[0..1], "P_")){
+                    std.debug.print("{s}\n", .{ entry.name });
+                }
+            }
         }
         std.debug.print("looking at {s}\n", .{cmd.target});
     } else if (std.mem.eql(u8, cmd.root, "s")){
